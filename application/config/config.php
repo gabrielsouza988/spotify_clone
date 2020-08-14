6f92852f25562ede769f82c66ab99a12aa755389
gabrielsouza988 <gabriel.souza988@hotmail.com>
2020-08-14 19:17:40 -0300

    moving files
    moving files

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/go_store/';
+$config['base_url'] = 'http://localhost/spotify_clone/';
 
 /*
 |--------------------------------------------------------------------------
